e03f9a5d68588c95fa2f7ff010ce0bb9a92242a4
anand bhavani  Krishnankutty <anandkrishnanbk@gmail.com>
2018-05-30 08:06:57 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'app';
+  private answer;
+  private positiveInteger;
+  private firstSingleDigit;
+  private secondSingleDigit;
+  private firstReminder;
+  private secondReminder;
+  ngOnInit() {
+    this.firstReminder = this.positiveInteger % this.firstSingleDigit;
+    this.secondReminder = this.positiveInteger % this.secondSingleDigit;
+  }
+  onSubmit() {
+    console.log(this.positiveInteger);
+    for (let i = 0; i < this.positiveInteger; i++) {
+        console.log(i);
+         this.answer = i + '&nbsp;';
+    }
+  }
+
+
 }
