7d2ee88855f8ef6635600afab2e716e0bb326a2f
anand bhavani  Krishnankutty <anandkrishnanbk@gmail.com>
2018-05-31 05:07:01 -0400

    added changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,44 @@ import { Component, OnInit } from '@angular/core';
 
 export class AppComponent implements OnInit {
   title = 'app';
-  private answer;
+  private answer = '';
   private positiveInteger;
   private firstSingleDigit;
   private secondSingleDigit;
   private firstReminder;
   private secondReminder;
   ngOnInit() {
-    this.firstReminder = this.positiveInteger % this.firstSingleDigit;
     this.secondReminder = this.positiveInteger % this.secondSingleDigit;
   }
   onSubmit() {
-    console.log(this.positiveInteger);
+    if (this.firstSingleDigit === this.secondSingleDigit) {
+      alert('first digit and second digit cant be same');
+      return;
+    }
+    if (this.firstSingleDigit < 2 || this.secondSingleDigit < 2) {
+      alert('Both first digit and second digit should be greater than 1');
+      return;
+    }
+    let myNum = '';
     for (let i = 0; i < this.positiveInteger; i++) {
-        console.log(i);
-         this.answer = i + '&nbsp;';
+
+               if (i % this.firstSingleDigit === 0 && i % this.secondSingleDigit === 0) {
+                myNum = 'WATSON';
+               } else if (i % this.firstSingleDigit === 0 && i % this.secondSingleDigit !== 0  && i > this.firstSingleDigit) {
+                myNum = 'WAT';
+               } else if (i % this.firstSingleDigit !== 0 && i % this.secondSingleDigit === 0 && i > this.secondSingleDigit) {
+                myNum = 'SON';
+              } else {
+                if ((i + '').indexOf('' + this.firstSingleDigit) > -1 || (i + '').indexOf('' + this.secondSingleDigit) > -1) {
+                  myNum = i.toString();
+                }
+                myNum = i.toString();
+               }
+         if (i > 0) {
+          this.answer += myNum + ' ';
+         }
+         console.log(this.answer);
+
     }
   }
 
